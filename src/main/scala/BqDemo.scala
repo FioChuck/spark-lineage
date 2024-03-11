39101858c16cbccdfc04d03e8d7dde385f2815d4
FiocChuck <chasf@google.com>
2024-03-11 14:59:02 -0400

    refine query

--- src/main/scala/BqDemo.scala
+++ src/main/scala/BqDemo.scala
@@ -6,6 +6,8 @@ import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.functions._
+import com.google.cloud.spark.bigquery._
+import spark.implicits._
 
 object BqDemo {
   def main(args: Array[String]): Unit = {
@@ -26,75 +28,52 @@ object BqDemo {
       // )
       .getOrCreate()
 
-    import spark.implicits._
-
     val pages = Seq("Google", "Amazon", "Microsoft")
 
     val df_24 =
       spark.read
-        .format("bigquery")
-        .option("table", "bigquery-public-data.wikipedia.pageviews_2024")
-        .load()
-        .filter(to_date(col("datehour")).between("2024-01-01", "2024-01-31"))
-        .filter($"title".isin(pages: _*))
+        .bigquery("bigquery-public-data.wikipedia.pageviews_2024")
+        .filter(to_date($"datehour").between("2024-01-01", "2024-01-31"))
         .withColumnRenamed("views", "views_24")
-        .limit(100)
 
     val df_23 =
       spark.read
-        .format("bigquery")
-        .option("table", "bigquery-public-data.wikipedia.pageviews_2023")
-        .load()
-        .filter(to_date(col("datehour")).between("2023-01-01", "2023-01-31"))
-        .filter($"title".isin(pages: _*))
+        .bigquery("bigquery-public-data.wikipedia.pageviews_2023")
+        .filter(to_date($"datehour").between("2023-01-01", "2023-01-31"))
         .withColumnRenamed("views", "views_23")
-        .limit(100)
 
     val df_22 =
       spark.read
-        .format("bigquery")
-        .option("table", "bigquery-public-data.wikipedia.pageviews_2022")
-        .load()
-        .filter(to_date(col("datehour")).between("2022-01-01", "2022-01-31"))
-        .filter($"title".isin(pages: _*))
+        .bigquery("bigquery-public-data.wikipedia.pageviews_2022")
+        .filter(to_date($"datehour").between("2022-01-01", "2022-01-31"))
         .withColumnRenamed("views", "views_22")
-        .limit(100)
 
     val df_out = df_24
       .join(df_23, Seq("title"), "inner")
       .join(df_22, Seq("title"), "inner")
+      .filter($"title".isin(pages: _*))
       .groupBy("title")
       .agg(sum("views_22"), sum("views_23"), sum("views_24"))
       .withColumn(
         "max_views_jan",
-        greatest(
-          col("sum(views_22)"),
-          col("sum(views_23)"),
-          col("sum(views_24)")
-        )
+        greatest($"sum(views_22)", $"sum(views_23)", $"sum(views_24)")
       )
       .drop(
         "sum(views_22)",
         "sum(views_23)",
         "sum(views_24)"
       )
-      .withColumnRenamed("title", "page")
-      .withColumnRenamed("max_views_jan", "max_views")
 
-    // df_out.show()
+    df_out.show();
 
-    // print("done")
+    print("test")
 
     df_out.write
       .format("bigquery")
-      // .option(
-      //   "temporaryGcsBucket",
-      //   "cf-spark-temp"
-      // ) // indirect mode destination gcs bucket
       .option("writeMethod", "direct")
-      .mode("overwrite") // overwrite or append to destination table
+      .mode("overwrite")
       .save(
         "cf-data-analytics.dataproc.destination"
-      ) // define destination table
+      )
   }
 }
